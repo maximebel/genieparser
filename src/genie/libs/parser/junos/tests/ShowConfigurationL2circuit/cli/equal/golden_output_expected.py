41759be0828ec5833803610bbe0285f60004758d
zakaria tayq <zakaria.tayq@orange.com>
2022-06-03 16:00:02 +0200

    add testcase l2xconnect

---
+++ src/genie/libs/parser/junos/tests/ShowConfigurationL2circuit/cli/equal/golden_output_expected.py
@@ -0,0 +1,17 @@
+expected_output={
+    "configuration": {
+        "protocols": { 
+            "l2circuit": { 
+                "local-switching": {
+                    "interface" : [         
+                        {
+                        "name" : 'ge-0/0/1.123',
+                        "end-interface" : {  "interface" : ' ge-0/0/2.789' },
+                            "description" : 'PSA-AWS-LX789',
+                        }
+                                    ]                                                                                                                         
+                                }
+                        }
+                            }                                                  
+                    }
+                 }
